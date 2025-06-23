45a40116d386ac3dfa41a0e9bd194b5528c86919
WeiFuAndy23 <1597498880fw@gmail.com>
2025-06-23 15:56:01 -0400

    Initial Commit

---
+++ 单词Tests/__Tests.swift
@@ -0,0 +1,17 @@
+//
+//  __Tests.swift
+//  单词Tests
+//
+//  Created by weifu on 6/23/25.
+//
+
+import Testing
+@testable import __
+
+struct __Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
